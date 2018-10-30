23aa361093f636b4df142421568ad45d1b5ff27d
ja153903 <jaimeabbariao@gmail.com>
2018-10-30 17:37:38 -0400

    Going through more of the angular lessons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div>
+    <app-simple-form></app-simple-form>
+    {{mail.message}}
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'My first angular2 application';
+  title = 'My first angular2 application'
+
+  constructor (
+    @Inject('mail') private mail,
+    @Inject('api') private api
+  ) {}
 }
